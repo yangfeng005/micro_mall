afc8f5b83558cabc5b66c90b66b5a0141b4fb985
yangfeng <yang-feng@travelsky.com>
2021-02-19 15:25:59 +0800

    商品规格

---
+++ mall-shop/src/main/java/com/mall/shop/dao/gen/SpecificationGeneratedMapper.java
@@ -0,0 +1,17 @@
+package com.mall.shop.dao.gen;
+
+import com.backstage.core.mapper.BaseGeneratedMapper;
+import com.mall.shop.entity.customized.SpecificationAO;
+import com.mall.shop.entity.gen.SpecificationCriteria;
+
+/**
+ * 自动生成的 Specification 数据存取接口.
+ *
+ * <p>
+ * 该类于 2021-02-19 15:06:09 生成，请勿手工修改！
+ * </p>
+ * @author yangfeng
+ * @version 1.0.0, Feb 19, 2021
+ */
+public interface SpecificationGeneratedMapper extends BaseGeneratedMapper<SpecificationAO, SpecificationCriteria> {
+}
